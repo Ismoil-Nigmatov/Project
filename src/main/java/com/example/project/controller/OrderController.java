1675ed005e8027aa32a9ba57619a67c7e0318002
Ismoil-Nigmatov <ismoilnigmatov98@gmail.com>
2022-11-22 14:46:06 +0500

    Changes

--- src/main/java/com/example/project/controller/OrderController.java
+++ src/main/java/com/example/project/controller/OrderController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
-public class OrderController {
+@CrossOrigin(origins = {"*"},maxAge = 4800,allowCredentials = "false")public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
