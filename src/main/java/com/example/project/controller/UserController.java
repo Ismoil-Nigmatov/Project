f648012aba667f3eb378754d39bb8350b4130b64
Ismoil-Nigmatov <ismoilnigmatov98@gmail.com>
2022-12-06 15:39:27 +0500

    Changes

--- src/main/java/com/example/project/controller/UserController.java
+++ src/main/java/com/example/project/controller/UserController.java
@@ -32,32 +32,32 @@ public class UserController {
        ApiResponse<?> response = userService.getAll();
        return ResponseEntity.ok(response);
     }
-//
-//    @PreAuthorize(value = "hasAnyAuthority('ADMIN','USER')")
-//    @PostMapping("/profile/photo/{email}")
-//    public ResponseEntity<?> profilePhoto(@PathVariable String email,@RequestPart MultipartFile multipartFile) throws IOException {
-//        ApiResponse<?> response= userService.photo(email,multipartFile);
-//        return ResponseEntity.status(response.isSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(response);
-//    }
-//
-//    @PreAuthorize(value = "hasAnyAuthority('ADMIN','USER')")
-//    @GetMapping("/{email}")
-//    public ResponseEntity<?> getUser(@PathVariable String email){
-//        ApiResponse<?> response=userService.getOne(email);
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @PutMapping("/data/{email}")
-//    @PreAuthorize(value = "hasAnyAuthority('ADMIN','USER')")
-//    public ResponseEntity<?> editData(@PathVariable String email, @RequestBody UpdateUserDTO updateUserDTO){
-//        ApiResponse<?> response=userService.updateData(email,updateUserDTO);
-//        return ResponseEntity.status(response.isSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(response);
-//    }
-//
-//    @PutMapping("/password/{email}")
-//    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
-//    public ResponseEntity<?> editPassword(@PathVariable String email, @RequestBody PasswordDTO passwordDTO){
-//        ApiResponse<?> response=userService.updatePassword(email,passwordDTO);
-//        return ResponseEntity.status(response.isSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(response);
-//    }
+
+    @PreAuthorize(value = "hasAnyAuthority('ADMIN','USER')")
+    @PostMapping("/profile/photo/{email}")
+    public ResponseEntity<?> profilePhoto(@PathVariable String email,@RequestPart MultipartFile multipartFile) throws IOException {
+        ApiResponse<?> response= userService.photo(email,multipartFile);
+        return ResponseEntity.status(response.isSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(response);
+    }
+
+    @PreAuthorize(value = "hasAnyAuthority('ADMIN','USER')")
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getUser(@PathVariable String email){
+        ApiResponse<?> response=userService.getOne(email);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/data/{email}")
+    @PreAuthorize(value = "hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<?> editData(@PathVariable String email, @RequestBody UpdateUserDTO updateUserDTO){
+        ApiResponse<?> response=userService.updateData(email,updateUserDTO);
+        return ResponseEntity.status(response.isSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(response);
+    }
+
+    @PutMapping("/password/{email}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<?> editPassword(@PathVariable String email, @RequestBody PasswordDTO passwordDTO){
+        ApiResponse<?> response=userService.updatePassword(email,passwordDTO);
+        return ResponseEntity.status(response.isSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(response);
+    }
 }
