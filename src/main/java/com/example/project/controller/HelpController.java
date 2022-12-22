95f20b38e5bfc0882e7baf20c9395cf19bf898f3
Ismoil-Nigmatov <ismoilnigmatov98@gmail.com>
2022-12-22 17:21:06 +0500

    Changes

--- src/main/java/com/example/project/controller/HelpController.java
+++ src/main/java/com/example/project/controller/HelpController.java
@@ -6,7 +6,11 @@ import com.example.project.service.SupportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.Email;
 
 /**
  * @author "ISMOIL NIGMATOV"
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/help")
 @RequiredArgsConstructor
 @CrossOrigin(origins = {"*"},maxAge = 4800,allowCredentials = "false")
+@Validated
 public class HelpController {
 
     private final SupportService supportService;
@@ -29,8 +34,14 @@ public class HelpController {
     }
 
     @PostMapping("/urgent/{email}")
-    public ResponseEntity<?> urgent(@PathVariable String email){
+    public ResponseEntity<?> urgent(@Email @PathVariable String email){
         ApiResponse<?> response = supportService.urgent(email);
         return ResponseEntity.status(response.isSuccess()? HttpStatus.OK:HttpStatus.CONFLICT).body(response);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleValidationExceptions(ConstraintViolationException ex) {
+        return "Value should be valid";
     }
 }
